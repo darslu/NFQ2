c17f07cba75b22b98f60da4aa8ff0599d2e0eef2
Darius <MacBook_Pro_Retina@Dariaus-MacBook-Pro.local>
2018-02-21 08:18:08 +0200

    timepicker

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -104,7 +104,7 @@
 @stop
 
 @section('javascript')
-
+    @parent
     <script src="{{ url('quickadmin/js') }}/timepicker.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.js"></script>
     <script src="https://cdn.datatables.net/select/1.2.0/js/dataTables.select.min.js"></script>    <script>
